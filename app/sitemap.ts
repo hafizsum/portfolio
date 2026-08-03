52e41e05a7f874cf695242348d28d7c64c879742
hafizsum <60566721+hafizsum@users.noreply.github.com>
2026-08-03 19:54:47 +0600

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://hafizur.dev";
+  const baseUrl = "https://hafiz.pro.bd";
 
   return [
     {
